51affbcf019b1b0bce8facaccf4d00d4818e4100
Victor Calidor <victor.calidor@gmail.com>
2023-11-06 23:47:42 -0300

    Getting better at tileMapping
    OLHA O MACAAAAAAAAAAAAAAAAAAAAACO

---
+++ Calidor The Frog/mapDesigns/Isometric tileSet2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Isometric tileSet2" tilewidth="32" tileheight="32" tilecount="35" columns="5">
+ <image source="../SPRITES/Isometric tileSet2.png" width="160" height="224"/>
+</tileset>
